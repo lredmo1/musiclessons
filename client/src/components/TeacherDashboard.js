bb297c06c3e76e10b692b8a57454efadcd59bdb9
Lisa <redlisa125@gmail.com>
2021-12-17 08:11:00 -1000

    keypress console logging

--- client/src/components/TeacherDashboard.js
+++ client/src/components/TeacherDashboard.js
@@ -1,16 +1,49 @@
-import {SheetMusic} from './SheetMusic';
-import MusicToolBar from './MusicToolBar';
-import { useState } from 'react'
-
-
+import { SheetMusic } from "./SheetMusic";
+import MusicToolBar from "./MusicToolBar";
+import { useState } from "react";
 
 function TeacherDashboard() {
-  const[staves, setStaves] = useState([['g3', 'd4', 'e4', 'd4'], ['a4', 'd4', 'e4', 'd4'],['g3', 'd4', 'e4', 'd4'], ['a4', 'd4', 'e4', 'd4']])
+  const [staves, setStaves] = useState([
+    ["c4", "d4", "e4", "d4"],
+    ["a4", "d4", "e4", "d4"],
+    ["g3", "d4", "e4", "d4"],
+    ["a4", "d4", ["e4", 2]],
+  ]);
 
-    return (<>
-    <SheetMusic staves={staves} />
-    <MusicToolBar />
-    </>)
-  }
+  let musicNotes = [];
+
+  function handleKeyPress(e) {
+    if (e.key === "a") {
+    musicNotes.push("a4")
+      } else if (e.key === "b") {
+        musicNotes.push("b4")
+     } else if (e.key === "c") {
+        musicNotes.push("c4")
+      } else if (e.key === "d") {
+        musicNotes.push("d4")
+      } else if (e.key === "e") {
+        musicNotes.push("e4")
+      } else if (e.key === "f") {
+        musicNotes.push("f4")
+      } else if (e.key === "g") {
+        musicNotes.push("g4")
+      }
+      console.log(musicNotes);
+    }
   
-  export default TeacherDashboard;
+
+    
+  
+
+  return (
+    <>
+      <SheetMusic staves={staves} />
+      <form onKeyPress={handleKeyPress}>
+        <input type="text" />
+      </form>
+      <MusicToolBar />
+    </>
+  );
+}
+
+export default TeacherDashboard;
