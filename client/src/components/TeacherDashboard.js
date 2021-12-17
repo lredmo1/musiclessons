5ba4b48d4217fb16bdbd36892b65aa6140c3e822
Lisa <redlisa125@gmail.com>
2021-12-17 11:30:16 -1000

    console logging key presses

--- client/src/components/TeacherDashboard.js
+++ client/src/components/TeacherDashboard.js
@@ -10,30 +10,18 @@ function TeacherDashboard() {
     ["a4", "d4", ["e4", 2]],
   ]);
 
+  // ["c4", "d4", "e4", "d4"],
+  //   ["a4", "d4", "e4", "d4"],
+  //   ["g3", "d4", "e4", "d4"],
+  //   ["a4", "d4", ["e4", 2]];
+
   let musicNotes = [];
 
   function handleKeyPress(e) {
-    if (e.key === "a") {
-    musicNotes.push("a4")
-      } else if (e.key === "b") {
-        musicNotes.push("b4")
-     } else if (e.key === "c") {
-        musicNotes.push("c4")
-      } else if (e.key === "d") {
-        musicNotes.push("d4")
-      } else if (e.key === "e") {
-        musicNotes.push("e4")
-      } else if (e.key === "f") {
-        musicNotes.push("f4")
-      } else if (e.key === "g") {
-        musicNotes.push("g4")
-      }
-      console.log(musicNotes);
-    }
-  
-
-    
-  
+    musicNotes.push(`${e.key}4`);
+    console.log(musicNotes);
+    // setStaves(musicNotes);
+  }
 
   return (
     <>
