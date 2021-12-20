fb82cfaef5edd1aeb0be873caa00d92a0364ac6f
Lisa <redlisa125@gmail.com>
2021-12-20 11:03:04 -1000

    initial css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import { useHistory } from "react-router-dom";
+import styled from 'styled-components'; 
 
 function Navbar({teacher, setTeacher}) {
   let history = useHistory();
@@ -15,19 +16,31 @@ function Navbar({teacher, setTeacher}) {
     });
   }
 
-    return <>
-    <Link to="/" className="nav-links"> Home </Link>
+    return (<NavbarStyle>
+    <Link to="/" className="nav-links" style={{textDecoration: 'none'}}> Home </Link>
     {teacher? 
       <>
-      <Link to="/dashboard" className="nav-links"> Dashboard </Link>
+      <Link to="/dashboard" className="nav-links" style={{textDecoration: 'none'}}> Dashboard </Link>
       <div onClick={handleLogout}>Log Out</div> 
       </>:
       <>
-        <Link to="/signup" className="nav-links"> Sign Up </Link>
-        <Link to="/login" className="nav-links"> {teacher ? "Log Out" : "Log In"} </Link>
+        <Link to="/signup" className="nav-links" style={{textDecoration: 'none'}}> Sign Up </Link>
+        <Link to="/login" className="nav-links" style={{textDecoration: 'none'}}> {teacher ? "Log Out" : "Log In"} </Link>
       </>
     }
-    </>;
+    </NavbarStyle>)
   }
   
   export default Navbar;
+
+  const NavbarStyle = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 100px);
+  place-items: center;
+  justify-content: end;
+  text-decoration: none;
+  a:visited {
+    color: black;
+  }
+  cursor: pointer
+  `
--- client/src/components/TeacherSignUp.js
+++ client/src/components/TeacherSignUp.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import styled from "styled-components";
 
-function TeacherSignUp({setTeacher}) {
+function TeacherSignUp({ setTeacher }) {
   const [userFullName, setUserFullName] = useState("");
   const [userEmail, setUserEmail] = useState("");
   const [username, setUsername] = useState("");
@@ -31,7 +32,7 @@ function TeacherSignUp({setTeacher}) {
       setIsLoading(false);
       if (resp.ok) {
         resp.json().then((user) => setTeacher(null));
-        history.push("/login")
+        history.push("/login");
       } else {
         resp.json().then((data) => setErrors(data.errors));
       }
@@ -40,76 +41,94 @@ function TeacherSignUp({setTeacher}) {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className="input">
-          <label>
-            Full Name:{" "}
-            <input
-              type="text"
-              className="user-full-name"
-              autoComplete="off"
-              value={userFullName}
-              onChange={(e) => setUserFullName(e.target.value)}
-            ></input>
-          </label>
+      <FormStyle>
+        <form onSubmit={handleSubmit}>
+          <div className="input">
+            <label>
+              Full Name:{" "}
+              <input
+                type="text"
+                className="user-full-name"
+                autoComplete="off"
+                value={userFullName}
+                onChange={(e) => setUserFullName(e.target.value)}
+              ></input>
+            </label>
+          </div>
+          <div className="input">
+            <label>
+              Email:{" "}
+              <input
+                type="text"
+                className="user-email"
+                autoComplete="off"
+                value={userEmail}
+                onChange={(e) => setUserEmail(e.target.value)}
+              ></input>
+            </label>
+          </div>
+          <div className="input">
+            <label>
+              Username:{" "}
+              <input
+                type="text"
+                className="username"
+                autoComplete="off"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              ></input>
+            </label>
+          </div>
+          <div className="input">
+            <label>
+              Password:{" "}
+              <input
+                type="password"
+                className="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </label>
+          </div>
+          <div className="input">
+            <label>
+              Password Confirmation:
+              <input
+                type="password"
+                className="password-confirmation"
+                autoComplete="current-password"
+                value={passwordConfirmation}
+                onChange={(e) => setPasswordConfirmation(e.target.value)}
+              ></input>
+            </label>
+          </div>
+          <div className="button-submit">
+            <button type="submit">
+              {isLoading ? "Loading..." : "Sign Up"}
+            </button>
+          </div>
+        </form>
+        <div className="error-wrapper">
+          {errors.length > 0 &&
+            errors.map((error) => <p key={error}>{error}</p>)}
         </div>
-        <div className="input">
-          <label>
-            Email:{" "}
-            <input
-              type="text"
-              className="user-email"
-              autoComplete="off"
-              value={userEmail}
-              onChange={(e) => setUserEmail(e.target.value)}
-            ></input>
-          </label>
-        </div>        
-        <div className="input">
-          <label>
-            Username:{" "}
-            <input
-              type="text"
-              className="username"
-              autoComplete="off"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            ></input>
-          </label>
-        </div>
-        <div className="input">
-          <label>
-            Password:{" "}
-            <input
-              type="password"
-              className="password"
-              autoComplete="current-password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-          </label>
-        </div>
-        <div className="input">
-          <label>
-            Password Confirmation:
-            <input
-              type="password"
-              className="password-confirmation"
-              autoComplete="current-password"
-              value={passwordConfirmation}
-              onChange={(e) => setPasswordConfirmation(e.target.value)}
-            ></input>
-          </label>
-        </div>
-        <div className="button-submit">
-          <button type="submit">{isLoading ? "Loading..." : "Sign Up"}</button>
-        </div>
-      </form>
-      <div className="error-wrapper">
-        {errors.length > 0 && errors.map((error) => <p key={error}>{error}</p>)}
-      </div>
+      </FormStyle>
     </>
   );
 }
 
 export default TeacherSignUp;
+
+const FormStyle = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, 100px);
+  grid-template-columns: 100px;
+  justify-content: center;
+  button {
+    margin-top: 15px;
+  }
+  div {
+    margin-top: 15px;
+  }
+`;
