fb87f2c85232e91f387e240d644d8e256389b89c
Lisa <redlisa125@gmail.com>
2021-12-22 09:25:39 -1000

    moved music stuff from td to mc

--- client/src/components/MusicContainer.js
+++ client/src/components/MusicContainer.js
@@ -1,5 +1,57 @@
+import { SheetMusic } from "./SheetMusic";
+import MusicToolBar from "./MusicToolBar";
+import { useState } from "react";
+import Piano from "./Piano";
+
 function MusicContainer() {
-    return <></>;
+  const [staves, setStaves] = useState([]);
+
+  function handleKeyPress(e) {
+    const regex = /[^a-g]/gi;
+    e.target.value = e.target.value.replace(regex, "");
+    let musicNotesArray = [...staves];
+    if (
+      musicNotesArray[musicNotesArray.length - 1] &&
+      musicNotesArray[musicNotesArray.length - 1].length < 4
+    ) {
+      if (e.key === "o") {
+        musicNotesArray[musicNotesArray.length - 1].push("c5");
+      } else if (e.key === "Backspace") {
+        musicNotesArray[musicNotesArray.length - 1].pop();
+      } else if (
+        e.key === "a" ||
+        e.key === "b" ||
+        e.key === "c" ||
+        e.key === "d" ||
+        e.key === "e" ||
+        e.key === "f" ||
+        e.key === "g"
+      ) {
+        musicNotesArray[musicNotesArray.length - 1].push(`${e.key}4`);
+      }
+      setStaves(musicNotesArray);
+    } else {
+      let musicNotes = [];
+      if (e.key === "o") {
+        musicNotes.push("c5");
+      } else if (e.key === "Backspace") {
+        musicNotes.pop();
+      } else {
+        musicNotes.push(`${e.key}4`);
+      }
+      setStaves((currentstate) => [...currentstate, musicNotes]);
+    }
   }
-  
-  export default MusicContainer;
+  return (
+    <>
+      <SheetMusic staves={staves} />
+      <form onKeyUp={handleKeyPress}>
+        <input type="text" placeholder=" Click to begin" />
+      </form>
+      <MusicToolBar />
+      <Piano />;
+    </>
+  );
+}
+
+export default MusicContainer;
--- client/src/components/TeacherDashboard.js
+++ client/src/components/TeacherDashboard.js
@@ -1,49 +1,17 @@
-import { SheetMusic } from "./SheetMusic";
-import MusicToolBar from "./MusicToolBar";
-import { useState } from "react";
-import Piano from "./Piano";
+import MusicContainer from "./MusicContainer";
 import styled from "styled-components";
 
 function TeacherDashboard() {
-  const [staves, setStaves] = useState([]);
-
-  function handleKeyPress(e) {
-    const regex = /[^a-g]/gi;
-    e.target.value = e.target.value.replace(regex, "")
-    let musicNotesArray = [...staves];
-    if (
-      musicNotesArray[musicNotesArray.length - 1] &&
-      musicNotesArray[musicNotesArray.length - 1].length < 4
-    ) {
-      if (e.key === "o") {
-        musicNotesArray[musicNotesArray.length - 1].push("c5");
-      } else if (e.key === "Backspace") {
-        musicNotesArray[musicNotesArray.length - 1].pop()
-      } else if (e.key === "a" || e.key === "b" || e.key === "c" || e.key === "d" || e.key === "e" || e.key === "f" || e.key === "g") {
-        musicNotesArray[musicNotesArray.length - 1].push(`${e.key}4`);
-      } 
-      setStaves(musicNotesArray);
-    } else {
-      let musicNotes = [];
-      if (e.key === "o") {
-        musicNotes.push("c5");
-      } else if (e.key === "Backspace") {
-        musicNotes.pop()
-      } else {
-        musicNotes.push(`${e.key}4`);
-      }
-      setStaves((currentstate) => [...currentstate, musicNotes]);
-    }
-  }
-
+ 
   return (
     <DashboardStyle>
-      <SheetMusic staves={staves} />
+      <MusicContainer />
+      {/* <SheetMusic staves={staves} />
       <form onKeyUp={handleKeyPress}>
         <input type="text" placeholder=" Click to begin"/>
       </form>
       <MusicToolBar />
-      <Piano />
+      <Piano /> */}
     </DashboardStyle>
   );
 }
