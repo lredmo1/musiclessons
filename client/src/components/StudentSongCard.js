14a4e6099426b95cb156a971c047a3d3977432fc
Lisa <redlisa125@gmail.com>
2021-12-28 16:03:32 -1000

    saved music sort of rendering in staff

--- client/src/components/StudentSongCard.js
+++ client/src/components/StudentSongCard.js
@@ -1,43 +1,37 @@
 import { useState } from "react";
-import MusicContainer from "./MusicContainer";
+import SavedMusicContainer from "./SavedMusicContainer";
 
 function StudentSongCard({ song, user }) {
   const [playSavedSong, setPlaySavedSong] = useState(false);
   const [staves, setStaves] = useState([]);
 
 
-  function handleKeyPress(e) {
-    const regex = /[^a-g]/gi;
-    e.target.value = e.target.value.replace(regex, "");
+  function handleKeyPress(note) {
     let musicNotesArray = [...staves];
     if (
       musicNotesArray[musicNotesArray.length - 1] &&
       musicNotesArray[musicNotesArray.length - 1].length < 4
     ) {
-      if (e.key === "o") {
+      if (note === "o") {
         musicNotesArray[musicNotesArray.length - 1].push("c5");
-      } else if (e.key === "Backspace") {
-        musicNotesArray[musicNotesArray.length - 1].pop();
       } else if (
-        e.key === "a" ||
-        e.key === "b" ||
-        e.key === "c" ||
-        e.key === "d" ||
-        e.key === "e" ||
-        e.key === "f" ||
-        e.key === "g"
+        note === "a" ||
+        note === "b" ||
+        note === "c" ||
+        note === "d" ||
+        note === "e" ||
+        note === "f" ||
+        note === "g"
       ) {
-        musicNotesArray[musicNotesArray.length - 1].push(`${e.key}4`);
+        musicNotesArray[musicNotesArray.length - 1].push(`${note}4`);
       }
       setStaves(musicNotesArray);
     } else {
       let musicNotes = [];
-      if (e.key === "o") {
+      if (note === "o") {
         musicNotes.push("c5");
-      } else if (e.key === "Backspace") {
-        musicNotes.pop();
       } else {
-        musicNotes.push(`${e.key}4`);
+        musicNotes.push(`${note}4`);
       }
       setStaves((currentstate) => [...currentstate, musicNotes]);
     }
@@ -48,7 +42,8 @@ function StudentSongCard({ song, user }) {
     setPlaySavedSong(true);
     let savedSong = song.data.split("")
     console.log(savedSong)
-    // savedSong.map((note) => handleKeyPress(note))
+    savedSong.map((note) => handleKeyPress(note))
+    console.log(staves)
 }
 
   function handleCancelPlaySong() {
@@ -61,7 +56,7 @@ function StudentSongCard({ song, user }) {
       {playSavedSong ? (
         <>
           <button onClick={handleCancelPlaySong}>Cancel</button>
-          {/* <MusicContainer staves={staves}/> */}
+          <SavedMusicContainer staves={staves}/>
         </>
       ) : (
         <button onClick={handlePlaySong}>View</button>
