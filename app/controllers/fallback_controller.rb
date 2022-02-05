991427fb4832f3a5894b7e5f12b47dc0ae4573fa
Lisa <redlisa125@gmail.com>
2022-02-04 18:36:12 -1000

    fixed file path

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,6 +1,6 @@
 class FallbackController < ActionController::Base
     def index
         puts `this is fallback controller. #{params[:path]}`
-        render file: 'client/public/index.html'
+        render file: 'public/index.html'
     end
 end
