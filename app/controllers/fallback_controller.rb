0037eec1812b0c963ffa93d6b26deaa7c78b5fc2
Lisa <redlisa125@gmail.com>
2022-02-04 18:27:13 -1000

    debugging routes

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,6 +1,6 @@
 class FallbackController < ActionController::Base
     def index
-        puts `this is fallback controller. #{[params:path]}`
+        puts `this is fallback controller. #{params[:path]}`
         render file: 'client/public/index.html'
     end
 end
